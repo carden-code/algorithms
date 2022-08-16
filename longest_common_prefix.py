dd0f6f0676d89f2ee42eba63f392e52356ff00d5
Vyacheslav <borisenkov89@yandex.ru>
2022-08-16 22:19:39 +0300

    init task

---
+++ longest_common_prefix.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def longest_common_prefix(self, strs: List[str]) -> str:
+    """longestCommonPrefix
+        Example 1:
+        Input: strs = ["flower","flow","flight"]
+        Output: "fl"
+        
+        Example 2:
+        Input: strs = ["dog","racecar","car"]
+        Output: ""
+        Explanation: There is no common prefix among the input strings.
+    """
+    pass
