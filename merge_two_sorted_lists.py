c0c79182ca0883d6ecaf73ba616cb6b387b64722
Vyacheslav <borisenkov89@yandex.ru>
2022-08-20 23:23:42 +0300

    init

---
+++ merge_two_sorted_lists.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def merge_two_lists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    """Takes the headers of two sorted linked lists list1 and list2.
+
+        Merges two lists into one sorted list. 
+        The list must be composed by joining the nodes of the first two lists.
+    """
+    pass
